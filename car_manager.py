b623337745e724fbe59b2fa3ba65adc078301e35
sanjay321654 <sanjayilaya2002@gmail.com>
2023-09-06 20:04:51 +0530

    updated main.py and scoreboard.py

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 import random
 
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple", "grey", "white"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
