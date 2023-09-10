46884ce2baa2e1af70304d22662a6db511c15b45
sanjay321654 <sanjayilaya2002@gmail.com>
2023-09-10 12:26:36 +0530

    updated main.py and scoreboard.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 from scoreboard import Scoreboard
+
 score = Scoreboard()
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -28,8 +29,8 @@ while game_is_on:
         if car.distance(player) < 28:
             game_is_on = False
             score.game_over()
-    # If the player reaches the destination then back the started position.
 
+    # If the player reaches the destination then back the started position.
     if player.ycor() == player.is_finish:
         player.reset_position()
         score.increase_score()
